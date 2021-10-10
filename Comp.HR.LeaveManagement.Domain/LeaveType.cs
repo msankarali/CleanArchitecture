1845e7fdeaea979d532812a3a884954387a1cc0d
Muharrem Servet Ankarali <mservetankarali@gmail.com>
2021-10-10 13:13:39 +0300

    Add project files.

---
+++ Comp.HR.LeaveManagement.Domain/LeaveType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Comp.HR.LeaveManagement.Domain.Common;
+
+namespace Comp.HR.LeaveManagement.Domain
+{
+    public class LeaveType : BaseDomainEntity
+    {
+        public string Name { get; set; }
+        public int DefaultDays { get; set; }
+    }
+}
