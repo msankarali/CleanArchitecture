077d492747ac050c398bf91d063dce79c78b301d
Muharrem Servet Ankarali <mservetankarali@gmail.com>
2021-10-10 13:21:50 +0300

    add generic and domain repositories

---
+++ Comp.HR.LeaveManagement.Application/Persistence/Contracts/ILeaveRequestRepository.cs
@@ -0,0 +1,9 @@
+﻿using Comp.HR.LeaveManagement.Domain;
+
+namespace Comp.HR.LeaveManagement.Application.Persistence.Contracts
+{
+    public interface ILeaveRequestRepository : IGenericRepository<LeaveRequest>
+    {
+        
+    }
+}
---
+++ Comp.HR.LeaveManagement.Application/Persistence/Contracts/ILeaveTypeRepository.cs
@@ -0,0 +1,9 @@
+﻿using Comp.HR.LeaveManagement.Domain;
+
+namespace Comp.HR.LeaveManagement.Application.Persistence.Contracts
+{
+    public interface ILeaveTypeRepository : IGenericRepository<LeaveType>
+    {
+        
+    }
+}
