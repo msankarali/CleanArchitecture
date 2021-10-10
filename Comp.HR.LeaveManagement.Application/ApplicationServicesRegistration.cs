9b33a71fd825e20d998bc9111d427bed500d457c
Muharrem Servet Ankarali <mservetankarali@gmail.com>
2021-10-10 13:42:28 +0300

    add mediatr n update service

--- Comp.HR.LeaveManagement.Application/ApplicationServicesRegistration.cs
+++ Comp.HR.LeaveManagement.Application/ApplicationServicesRegistration.cs
@@ -2,15 +2,19 @@
 using System.Collections.Generic;
 using System.Reflection;
 using System.Text;
+using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Comp.HR.LeaveManagement.Application
 {
     public static class ApplicationServicesRegistration
     {
-        public static void ConfigureApplicationServices(this IServiceCollection services)
+        public static IServiceCollection ConfigureApplicationServices(this IServiceCollection services)
         {
             services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+
+            return services;
         }
     }
 }
